248953e9bac58bf1ddffc6d53d3723f2bcc1a3fe
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-03-11 19:47:18 -0700

    Updated Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,22 +3,24 @@ import { FaDiscord, FaGithub, FaLinkedin, FaPaperPlane } from "react-icons/fa";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-4 max-w-lg">
+    <section className="flex flex-col gap-4 max-w-[45ch]">
       <h1 className="font-bold text-5xl place-self-start">
         Tomas <span className="text-contrast">Oh</span>
       </h1>
       <p>
-        Currently pursuing a bachelor's in Computer Science with a minor in
-        Mathematics. I find joy in exploring new technologies and building
-        applications.
+        Currently pursuing a bachelor's in{" "}
+        <span className="font-semibold">Computer Science</span> at California
+        State University, Fullerton , with a minor in Mathematics. I find joy in{" "}
+        <span className="font-semibold">exploring</span> technologies and{" "}
+        <span className="font-semibold">building</span> applications!
       </p>
       <p>
         In my free time, I like rock climbing, playing soccer, drawing, and
         hanging out with friends.
       </p>
-      <p>
-        Reach out to me if you would like to talk! Thanks for looking at my
-        portfolio :)
+      <p className="font-semibold">
+        Reach out to me if you would like to talk! Thanks for taking a look at
+        my portfolio :)
       </p>
 
       <div className="flex gap-3">
