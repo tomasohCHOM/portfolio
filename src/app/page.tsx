01752e781b973b9611f7a2d039d1c5a5ac4141f0
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-03-20 18:45:13 -0700

    Change order of components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         <Navbar />
         <div className="mt-8">
           <Hero />
-          <Experience />
           <Projects />
+          <Experience />
         </div>
       </main>
       <Footer />
