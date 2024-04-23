fb1e50fb6883940ff596c93acb7bd5b16eca7028
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-23 09:53:56 -0700

    Blur images

--- src/components/Extra.tsx
+++ src/components/Extra.tsx
@@ -17,8 +17,9 @@ export default function Extra() {
       <h2 className="mt-8 text-3xl font-semibold">Extra</h2>
       <div className="relative mt-6 flex items-center justify-center">
         {[...pics, ...pics, ...pics, ...pics].map((pic, i) => {
-          let zIndex = `z-${40 - (i + 1) * 10}`;
-          let pos = i === 0 ? "" : "absolute";
+          const zIndex = `z-${40 - (i + 1) * 10}`;
+          const rotation = i % 2 === 0 ? "rotate-[6deg]" : "";
+          const pos = i === 0 ? "" : "absolute";
           return (
             <Image
               key={`${pic}-${i}`}
@@ -26,7 +27,7 @@ export default function Extra() {
               alt={`${pic} picture`}
               width={500}
               height={500}
-              className={`${zIndex} ${pos} max-w-52 rounded-lg drop-shadow-sm`}
+              className={`${zIndex} ${pos} ${rotation} max-w-52 rounded-lg blur-[2px] drop-shadow-sm transition hover:blur-none`}
             />
           );
         })}
