bdb4a0fa89d8187ae27a018f5be075da078fba42
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-01-19 12:04:28 -0800

    wip

--- src/pages/v1.jsx
+++ src/pages/v1.jsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 import Hero from "@/components/v1/Hero";
+import { useTheme } from "@/theme";
 import "@/styles.css";
 
 export default function PortfolioV1() {
+  useTheme("v1");
   return (
-    <div className="v1 h-screen">
+    <div>
       <Header />
       <main className="px-8 py-0 md:py-12">
         <div className="max-w-160">
