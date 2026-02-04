60f0151e6fae1dd8a5da06b89f8d9e0f2a2d9763
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-02-04 11:47:31 -0800

    add webring links

--- src/components/v1/Footer.jsx
+++ src/components/v1/Footer.jsx
@@ -1,6 +1,7 @@
 import { motion } from "motion/react";
 import { containerVariantsV1 } from "@/motions";
 import { YEAR } from "@/year";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 
 export default function Footer() {
   return (
@@ -9,9 +10,23 @@ export default function Footer() {
       whileInView="visible"
       viewport={{ once: true }}
       variants={containerVariantsV1}
-      className="mt-auto px-8 pt-8 pb-12 md:pb-8 text-xs text-text lg:text-text-blur"
+      className="mt-auto px-8 pt-8 pb-12 md:pb-8 text-text lg:text-text-blur"
     >
-      @{YEAR} Tomas Oh
+      <div className="max-w-max flex items-center gap-2">
+        <a
+          href="https://webring-1.vercel.app/?from=tomasohCHOM&dir=prev"
+          rel="noopener noreferrer"
+        >
+          <BiChevronLeft size={20} className="hover:text-text transition" />
+        </a>
+        <span className="text-xs">© {YEAR} Tomas Oh</span>
+        <a
+          href="https://webring-1.vercel.app/?from=tomasohCHOM&dir=next"
+          rel="noopener noreferrer"
+        >
+          <BiChevronRight size={20} className="hover:text-text transition" />
+        </a>
+      </div>
     </motion.footer>
   );
 }
